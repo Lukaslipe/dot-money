ebb78c1ca276890a4554e6b650379456de5958ab
unknown <mylenasantos651@gmailcom>
2025-10-12 11:48:47 -0300

    Criando a classe Custos

---
+++ DotMoney.API/Models/Custos.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace DotMoney.API.Models;
+
+public class Custos
+    {
+        public int Id { get; set; }
+
+        public string Descricao { get; set; } = string.Empty;
+        public decimal Valor { get; set; }
+        public DateTime Data { get; set; }
+        
+        // Relacionamento com Categoria
+        public int CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
+
+        // Relacionamento com Usuário
+        public int UsuariosId { get; set; }
+        public Usuarios? Usuario { get; set; }
+
+        public DateTime DataRegistro { get; set; } = DateTime.Now;
+    }
