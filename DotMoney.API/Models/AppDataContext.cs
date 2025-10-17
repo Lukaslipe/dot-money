14010141811d0e9967727ac7bfb4b60592297473
lukaslipe <lucascristodev@gmail.com>
2025-10-17 19:43:18 -0300

    Adicionando a classe Log ao BD

--- DotMoney.API/Models/AppDataContext.cs
+++ DotMoney.API/Models/AppDataContext.cs
@@ -9,7 +9,7 @@ public class AppDataContext : DbContext
     public DbSet<Categoria> Categorias { get; set; }
     public DbSet<Custos> Custos { get; set; }
     public DbSet<Usuarios> Usuarios { get; set; }
-
+    public DbSet<Log> Log { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=dotMoney.db");
